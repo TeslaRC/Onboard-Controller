505ac848e07a39395b37fa8dc0c83c80cda1fea1
tesla15 <117595540+tesla15@users.noreply.github.com>
2023-08-22 00:11:02 +0200

    Update script.py

--- steering/script.py
+++ steering/script.py
@@ -1,5 +1,25 @@
 import socket
 import serial
+import subprocess
+import os
+
+# Define the resolutions and their corresponding GStreamer pipelines
+resolutions = {
+    0: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=430, height=254 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000",
+    1: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=537, height=315 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000",
+    2: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=645, height=378 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000",
+    3: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=753, height=441 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000",
+    4: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=861, height=504 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000",
+    5: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=969, height=567 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000",
+    6: "gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg, width=1024, height=768, framerate=30/1 ! jpegdec ! videoconvert ! videoscale ! video/x-raw, width=1024, height=598 ! jpegenc quality=40 ! rtpjpegpay ! udpsink host=172.26.24.83 port=5000"
+}
+
+def update_gstreamer_pipeline(resolution_index):
+    os.system("pkill gst-launch-1.0")
+
+    pipeline = resolutions.get(resolution_index)
+    if pipeline:
+        subprocess.Popen(pipeline, shell=True)
 
 def main():
     ip_address = '0.0.0.0'
@@ -16,8 +36,12 @@ def main():
             throttle_value = int.from_bytes(data, byteorder='big')
             print(f"Received Throttle Value: {throttle_value}")
 
-            throttle_str = str(throttle_value) + '\n'
-            serial_port.write(throttle_str.encode())
+            if received_value in resolutions:
+                update_gstreamer_pipeline(received_value)
+            else:
+                throttle_str = str(received_value) + '\n'
+                serial_port.write(throttle_str.encode())
+
 
 if __name__ == '__main__':
     main()
